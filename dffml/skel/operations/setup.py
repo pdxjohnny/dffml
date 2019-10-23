8fcf5fd461c87036c49aad6cfd5af2e41c352aea
John Andersen <john.s.andersen@intel.com>
2019-10-23 16:31:28 -0700

    skel: operations: Remove implemenetations
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- dffml/skel/operations/setup.py
+++ dffml/skel/operations/setup.py
@@ -4,14 +4,9 @@ from dffml_setup_common import SETUP_KWARGS, IMPORT_NAME
 
 SETUP_KWARGS["entry_points"] = {
     "dffml.operation": [
-        f"calc_add = {IMPORT_NAME}.operations:calc_add.op",
-        f"calc_mult = {IMPORT_NAME}.operations:calc_mult.op",
-        f"calc_parse_line = {IMPORT_NAME}.operations:calc_parse_line.op",
-    ],
-    "dffml.operation.implementation": [
-        f"calc_add = {IMPORT_NAME}.operations:calc_add.imp",
-        f"calc_mult = {IMPORT_NAME}.operations:calc_mult.imp",
-        f"calc_parse_line = {IMPORT_NAME}.operations:calc_parse_line.imp",
+        f"calc_add = {IMPORT_NAME}.operations:calc_add",
+        f"calc_mult = {IMPORT_NAME}.operations:calc_mult",
+        f"calc_parse_line = {IMPORT_NAME}.operations:calc_parse_line",
     ],
 }
 
