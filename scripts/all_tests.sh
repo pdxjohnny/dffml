99a98292ad9933f6d5004ea9bf5ecfa9ef152be3
John Andersen <johnandersenpdx@gmail.com>
2019-10-26 08:09:55 -0700

    updated all_tests.sh to run examples, docs, and create skels
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- scripts/all_tests.sh
+++ scripts/all_tests.sh
@@ -17,14 +17,52 @@ PLUGINS=("${SRC_ROOT}/" \
 	"${SRC_ROOT}/feature/auth" \
 	"${SRC_ROOT}/service/http" \
 	"${SRC_ROOT}/source/mysql")
-for CURR in ${PLUGINS[@]}; do
-  cd "${CURR}"
+for PLUGIN in ${PLUGINS[@]}; do
+  cd "${PLUGIN}"
   "${PYTHON}" setup.py test
   exit_code=$?
   if [ "x${exit_code}" == "x0" ]; then
-    echo "[PASS]: ${CURR}"
+    echo "[PASS]: ${PLUGIN}"
   else
-    echo "[FAIL]: ${CURR}"
+    echo "[FAIL]: ${PLUGIN}"
   fi
-	cd -
+  # Test example and skel if main
+  if [ "x${PLUGIN}" == "x${SRC_ROOT}/" ]; then
+    # Test examples
+    cd examples
+    "${PYTHON}" -m pip install -r requirements.txt
+    "${PYTHON}" -m unittest discover
+    cd -
+    # Generate docs
+    "${SRC_ROOT}/scripts/docs.sh"
+    # Create venv
+    TMP_DIR="$(mktemp -d)"
+    # Remove temporary directory when done
+    trap "rm --preserve-root=all -rf ${TMP_DIR}" EXIT
+    "${PYTHON}" -m venv "${TMP_DIR}/.venv"
+    cd "${TMP_DIR}"
+    # Activate venv
+    source "${TMP_DIR}/.venv/bin/activate"
+    # Ensure that the python we're using is from the venv
+    which "${PYTHON}" | grep -q "${TMP_DIR}"
+    # Install DFFML to venv
+    "${PYTHON}" -m pip install "${SRC_ROOT}/"
+    # Create plugins
+    SKELS=("model" \
+      "operations" \
+      "service" \
+      "source" \
+      "config")
+    for SKEL in ${SKELS[@]}; do
+      dffml service dev create "${SKEL}" "test-${SKEL}"
+      cd "test-${SKEL}"
+      "${PYTHON}" setup.py install
+      "${PYTHON}" setup.py test
+      cd -
+    done
+    # Deactivate venv
+    deactivate
+  fi
+  # Go back to main source directory
+  cd "${SRC_ROOT}"
 done
