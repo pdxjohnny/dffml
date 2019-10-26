a500262937d425821e9357caac5c5f5358610f8b
John Andersen <johnandersenpdx@gmail.com>
2019-10-26 08:14:52 -0700

    style: format with black
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/skel/operations/tests/test_operations.py
+++ dffml/skel/operations/tests/test_operations.py
@@ -17,19 +17,21 @@ class TestOperations(AsyncTestCase):
         calc_strings_check = {"add 40 and 2": 42, "multiply 42 and 10": 420}
         async with MemoryOrchestrator.withconfig({}) as orchestrator:
             async with orchestrator(dataflow) as octx:
-                async for ctx, results in octx.run({
-                    to_calc: [
-                        Input(
-                            value=to_calc,
-                            definition=calc_parse_line.op.inputs["line"],
-                        ),
-                        Input(
-                            value=[calc_add.op.outputs["sum"].name],
-                            definition=GetSingle.op.inputs["spec"],
-                        ),
-                    ]
-                    for to_calc in calc_strings_check.keys()
-                }):
+                async for ctx, results in octx.run(
+                    {
+                        to_calc: [
+                            Input(
+                                value=to_calc,
+                                definition=calc_parse_line.op.inputs["line"],
+                            ),
+                            Input(
+                                value=[calc_add.op.outputs["sum"].name],
+                                definition=GetSingle.op.inputs["spec"],
+                            ),
+                        ]
+                        for to_calc in calc_strings_check.keys()
+                    }
+                ):
                     ctx_str = (await ctx.handle()).as_string()
                     self.assertEqual(
                         calc_strings_check[ctx_str],
