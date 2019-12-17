d4d5fcd305f3bcaa22b15c92b9f4f0a745fcd35c
Aghin Shah Alin <aghinsa@gmail.com>
2019-12-18 01:16:58 +0530

    base_db

---
+++ dffml/db/base.py
@@ -0,0 +1,36 @@
+import abc
+from dffml.df.base import (BaseDataFlowObject,BaseDataFlowObjectContext)
+from typing import Any,List,Callable,Optional
+
+class BaseDatabaseContext(BaseDataFlowObjectContext):
+
+    @abc.abstractmethod
+    async def create_table(self,table_name : str)->None:
+        """
+        creates a table with name `table_name` if it doesn't exist
+        """
+    
+    @abc.abstractmethod
+    async def insert(self,table_name:str,data:Dict[str,Any])->None:
+        """
+        inserts values to corresponding
+            cols (according to position) to the table `table_name`
+        """
+        
+    @abc.abstractmethod
+    async def update(self,table_name:str,data:Dict[str,Any],
+            condition:Optional[Callable[...,bool]])->None:
+            """
+            updates values of rows (satisfying `condition` if provided) with 
+            `data` in `table_name`
+            """
+
+    @abc.abstractmethod
+    async def lookup(self,table_name:str,cols:List[str],
+        condition:Optional[Callable[...,bool]] )->List[Any]:
+        """
+        returns list of rows (satisfying `condition` if provided) from `table_name` 
+        """             
+
+#TODO add entrypoint here
+class BaseDatabase(BaseDataFlowObject):
