58cc0db82a77a3f137955c686b7f1a6643a5c23d
Aghin Shah Alin <aghinsa@gmail.com>
2019-12-19 22:38:12 +0530

    sanitize

--- dffml/db/base.py
+++ dffml/db/base.py
@@ -2,10 +2,45 @@ import abc
 from dffml.df.base import BaseDataFlowObject,BaseDataFlowObjectContext
 from typing import Any,List,Callable,Optional,Dict
 
-class BaseDatabaseContext(BaseDataFlowObjectContext):
+
+import abc
+import inspect
+import types
+
+def sanitize_non_bindable(*to_scrub):
+    
+    def scrub(obj):
+        if(isinstance(obj,str)):
+            return '_'.join(obj.split(" "))
+        if(isinstance(obj,Dict)):
+            nobj={ scrub(k):v for k,v in obj.items() }
+            return nobj
+        
+    def sanitize(func):
+        sig=inspect.signature(func)
+        def wrappper(*args,**kwargs):
+            bounded = sig.bind(*args,*kwargs)
+            for arg in to_scrub:
+                if arg in bounded.arguments:
+                    bounded.arguments[arg]=scrub(bounded.arguments[arg])
+            return func(*bounded.args , **bounded.kwargs)
+        return wrappper
+    return sanitize
+
+
+class DatabaseContextMeta(type):
+    def __init__(cls,name,bases,clsdict):
+        to_scrub=["cols","table_name","data"]
+        for f in clsdict:
+            if (not f.startswith("__")) and ( type(getattr(cls,f)) == types.FunctionType):
+                setattr(cls,f,sanitize_non_bindable(*to_scrub)(getattr(cls,f)))
+
+
+
+class BaseDatabaseContext(BaseDataFlowObjectContext,metaclass=DatabaseContextMeta):
 
     @abc.abstractmethod
-    async def create_table(self,table_name : str)->None:
+    async def create_table(self,table_name : str,cols:Dict[str,str])->None:
         """
         creates a table with name `table_name` if it doesn't exist
         """
@@ -19,7 +54,7 @@ class BaseDatabaseContext(BaseDataFlowObjectContext):
 
     @abc.abstractmethod
     async def update(self,table_name:str,data:Dict[str,Any],
-            condition:Optional[Callable[...,bool]])->None:
+            condition:"Optional[ Callable[...,bool]]" )->None:
             """
             updates values of rows (satisfying `condition` if provided) with 
             `data` in `table_name`
@@ -27,12 +62,12 @@ class BaseDatabaseContext(BaseDataFlowObjectContext):
 
     @abc.abstractmethod
     async def lookup(self,table_name:str,cols:List[str],
-        condition:Optional[Callable[...,bool]] )->List[Any]:
+        condition : "Optional[ Callable[...,bool]]" )->List[Any]:
         """
         returns list of rows (satisfying `condition` if provided) from `table_name` 
         """             
 
 #TODO add entrypoint here
-class BaseDatabase(BaseDataFlowObject):
+class BaseDatabaseObject(BaseDataFlowObject):
     """
     """
