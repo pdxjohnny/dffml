4308e3a2006277f2da4cb01b7d5e39b75367fd9e
John Andersen <john.s.andersen@intel.com>
2019-12-26 09:41:21 -0800

    run black
    Signed-off-by: John Andersen <john.s.andersen@intel.com>

--- dffml/db/base.py
+++ dffml/db/base.py
@@ -2,141 +2,156 @@ import abc
 import inspect
 import types
 
-from typing import Any,List,Callable,Optional,Dict,Tuple,Union
+from typing import Any, List, Callable, Optional, Dict, Tuple, Union
 from collections import namedtuple
 from functools import wraps
 
 
-from dffml.df.base import BaseDataFlowObject,BaseDataFlowObjectContext
+from dffml.df.base import BaseDataFlowObject, BaseDataFlowObjectContext
 from dffml.util.entrypoint import base_entry_point
 
 
-Condition=namedtuple('Condtion',['column','operation','value'])
-Conditions = Union [
-                    List[ List[ Condition] ],
-                    List[ List[ Tuple[str] ] ],
-                 ]
+Condition = namedtuple("Condtion", ["column", "operation", "value"])
+Conditions = Union[
+    List[List[Condition]], List[List[Tuple[str]]],
+]
 
 
 class DatabaseContextConstraint(abc.ABC):
-    def __init_subclass__(cls,**kwargs):
+    def __init_subclass__(cls, **kwargs):
         super().__init_subclass__(**kwargs)
         for attr in vars(cls).keys():
-            func = getattr(cls,attr)
+            func = getattr(cls, attr)
             if (
                 (not attr.startswith("__"))
                 and inspect.isfunction(func)
-                and not ( inspect.ismethod(func) and func.__self__ is cls  ) #checks if `@classmethod`
-
+                and not (
+                    inspect.ismethod(func) and func.__self__ is cls
+                )  # checks if `@classmethod`
             ):
-                setattr(cls,attr,cls.sanitize(func))
-    
+                setattr(cls, attr, cls.sanitize(func))
 
-class BaseDatabaseContext(BaseDataFlowObjectContext,DatabaseContextConstraint):
 
+class BaseDatabaseContext(
+    BaseDataFlowObjectContext, DatabaseContextConstraint
+):
     @classmethod
-    def sanitize_non_bindable(self,val):
+    def sanitize_non_bindable(self, val):
         if val.isalnum():
             return val
         raise ValueError(
-            f"`{val}` : Only alphanumeric [a-zA-Z0-9] characters are allowed as table,column names")
-    
-    @classmethod    
-    def sanitize(self,func):
-        sig=inspect.signature(func)
+            f"`{val}` : Only alphanumeric [a-zA-Z0-9] characters are allowed as table,column names"
+        )
+
+    @classmethod
+    def sanitize(self, func):
+        sig = inspect.signature(func)
+
         def scrub(obj):
-            if(isinstance(obj,str)):
+            if isinstance(obj, str):
                 return self.sanitize_non_bindable(obj)
-            if(isinstance(obj,Dict)):
-                nobj={ self.sanitize_non_bindable(k):v for k,v in obj.items() }
+            if isinstance(obj, Dict):
+                nobj = {
+                    self.sanitize_non_bindable(k): v for k, v in obj.items()
+                }
                 return nobj
-            if(isinstance(obj,List)):
-                nobj= list(map(scrub,obj))
+            if isinstance(obj, List):
+                nobj = list(map(scrub, obj))
                 return nobj
-            if(isinstance(obj,Condition)):
-                column,*others = obj 
-                nobj = Condition._make( [scrub(column),*others])
+            if isinstance(obj, Condition):
+                column, *others = obj
+                nobj = Condition._make([scrub(column), *others])
                 return nobj
             else:
                 return obj
 
         @wraps(func)
-        def wrappper(*args,**kwargs):
-            bounded = sig.bind(*args,*kwargs)
+        def wrappper(*args, **kwargs):
+            bounded = sig.bind(*args, *kwargs)
             for arg in bounded.arguments:
-                if (arg=='self' or arg=='cls'):
+                if arg == "self" or arg == "cls":
                     continue
-                if arg == 'conditions' :
-                    bounded.arguments[arg] = self.make_conditions(bounded.arguments[arg])  
-                bounded.arguments[arg]=scrub(bounded.arguments[arg])
-            return func(*bounded.args , **bounded.kwargs)
+                if arg == "conditions":
+                    bounded.arguments[arg] = self.make_conditions(
+                        bounded.arguments[arg]
+                    )
+                bounded.arguments[arg] = scrub(bounded.arguments[arg])
+            return func(*bounded.args, **bounded.kwargs)
+
         return wrappper
-    
+
     @classmethod
-    def make_conditions(self,lst):
-        if ( (not lst) or isinstance(lst[0][0] ,Condition)):
+    def make_conditions(self, lst):
+        if (not lst) or isinstance(lst[0][0], Condition):
             return lst
-        res = [ list(map(Condition._make,cnd)) for cnd in lst ]
+        res = [list(map(Condition._make, cnd)) for cnd in lst]
         return res
 
-    
     @classmethod
-    def make_condition_expression(self,conditions):
+    def make_condition_expression(self, conditions):
         def _make_condition_expression(conditions):
             def make_or(lst):
-                exp = [ f"({cnd.column} {cnd.operation} '{cnd.value}')"
-                        for cnd in lst
-                        ]
+                exp = [
+                    f"({cnd.column} {cnd.operation} '{cnd.value}')"
+                    for cnd in lst
+                ]
                 return " OR ".join(exp)
+
             def make_and(lst):
-                lst = [ f"({x})" for x in lst  ]
+                lst = [f"({x})" for x in lst]
                 return " AND ".join(lst)
 
-            lst = (map(make_or,conditions))
+            lst = map(make_or, conditions)
             lst = make_and(lst)
             return lst
 
         condition_exp = None
-        if (not conditions==None) and (len(conditions)!=0) :
+        if (not conditions == None) and (len(conditions) != 0):
             condition_exp = _make_condition_expression(conditions)
         return condition_exp
 
-
     @abc.abstractmethod
-    async def create_table(self,table_name:str ,cols:Dict[str,str])->None:
+    async def create_table(
+        self, table_name: str, cols: Dict[str, str]
+    ) -> None:
         """
         creates a table with name `table_name` if it doesn't exist
         """
-    
+
     @abc.abstractmethod
-    async def insert(self,table_name:str,data:Dict[str,Any]) -> None:
+    async def insert(self, table_name: str, data: Dict[str, Any]) -> None:
         """
         inserts values to corresponding
             cols (according to position) to the table `table_name`
         """
 
     @abc.abstractmethod
-    async def update(self,table_name:str,data:Dict[str,Any],
-            conditions:Conditions) -> None:
-            """
+    async def update(
+        self, table_name: str, data: Dict[str, Any], conditions: Conditions
+    ) -> None:
+        """
             updates values of rows (satisfying `condition` if provided) with 
             `data` in `table_name`
             """
 
     @abc.abstractmethod
-    async def lookup(self,table_name:str,cols:List[str],conditions:Conditions):
+    async def lookup(
+        self, table_name: str, cols: List[str], conditions: Conditions
+    ):
         """
         returns list of rows (satisfying `condition` if provided) from `table_name` 
-        """     
-    @abc.abstractmethod
-    async def remove(self,table_name:str,conditions:Conditions):
         """
-        """
-    
 
-@base_entry_point("dffml.db","db")
+    @abc.abstractmethod
+    async def remove(self, table_name: str, conditions: Conditions):
+        """
+        """
+
+
+@base_entry_point("dffml.db", "db")
 class BaseDatabaseObject(BaseDataFlowObject):
     """
     """
+
     def __call__(self) -> BaseDatabaseContext:
         return self.CONTEXT(self)
--- dffml/db/sqlitedb.py
+++ dffml/db/sqlitedb.py
@@ -1,129 +1,125 @@
 import asyncio
 import sqlite3
 import abc
-import operator 
+import operator
 
 
-from dffml.db.base import BaseDatabaseContext,BaseDatabaseObject,Condition,Conditions
+from dffml.db.base import (
+    BaseDatabaseContext,
+    BaseDatabaseObject,
+    Condition,
+    Conditions,
+)
 from dffml.base import config
-from dffml.df.base import BaseConfig,BaseDataFlowObject
+from dffml.df.base import BaseConfig, BaseDataFlowObject
 from dffml.util.entrypoint import entry_point, EntrypointNotFound
 
-from typing import Dict,Any,List,Union,Tuple
-
+from typing import Dict, Any, List, Union, Tuple
 
 
 @config
 class SqliteDatabaseConfig:
-    filename:str
+    filename: str
+
 
 class SqliteDatabaseContext(BaseDatabaseContext):
-    def __init__(self,parent:"SqliteDatabaseObject"):
+    def __init__(self, parent: "SqliteDatabaseObject"):
         self.parent = parent
-        
-        
-        
-    
-    async def create_table(self,table_name:str ,cols:Dict[str,str])->None:
+
+    async def create_table(
+        self, table_name: str, cols: Dict[str, str]
+    ) -> None:
         """
         creates a table with name `table_name` if it doesn't exist.
         arg `cols` : dict mapping column names to type of columns 
         """
 
-        query =( f"CREATE TABLE IF NOT EXISTS  {table_name} (" 
-                    + ','.join([ f"{k} {v}" for k,v in cols.items()])  
-                    + ")"
-                )
+        query = (
+            f"CREATE TABLE IF NOT EXISTS  {table_name} ("
+            + ",".join([f"{k} {v}" for k, v in cols.items()])
+            + ")"
+        )
 
         self.parent.cursor.execute(query)
-        
 
-    async def insert(self,table_name:str,data:Dict[str,Any]) -> None:
+    async def insert(self, table_name: str, data: Dict[str, Any]) -> None:
         """
         inserts values to corresponding
             cols (according to position) to the table `table_name`
         """
-        col_exp = ', '.join([f"'{col}'" for col in data])
-        query= (f"INSERT INTO {table_name} "
-              + f"( { col_exp } )" 
-              + f" VALUES( {', '.join('?' * len(data))} ) "
-            )  
+        col_exp = ", ".join([f"'{col}'" for col in data])
+        query = (
+            f"INSERT INTO {table_name} "
+            + f"( { col_exp } )"
+            + f" VALUES( {', '.join('?' * len(data))} ) "
+        )
         async with self.parent.lock:
             with self.parent.db:
-                self.parent.cursor.execute(query, 
-                                    list( data.values() )
-                                    )
-                
+                self.parent.cursor.execute(query, list(data.values()))
 
-    
-    async def update(self,table_name:str,data:Dict[str,Any],
-            conditions:Conditions) -> None:
+    async def update(
+        self, table_name: str, data: Dict[str, Any], conditions: Conditions
+    ) -> None:
         """
         updates values of rows (satisfying `condition` if provided) with 
         `data` in `table_name`
         """
         condition_exp = self.make_condition_expression(conditions)
 
-        query = (f"UPDATE {table_name} SET " 
-                + ' '.join([f"'{col}' = ?" for col in data])
-                + (f" WHERE {condition_exp}" if condition_exp is not None else "")
-            )
-       
+        query = (
+            f"UPDATE {table_name} SET "
+            + " ".join([f"'{col}' = ?" for col in data])
+            + (f" WHERE {condition_exp}" if condition_exp is not None else "")
+        )
+
         async with self.parent.lock:
             with self.parent.db:
-                self.parent.cursor.execute(query,list(data.values()))
-        return 
-            
+                self.parent.cursor.execute(query, list(data.values()))
+        return
 
-    async def lookup(self,table_name:str,cols:List[str],conditions:Conditions):
+    async def lookup(
+        self, table_name: str, cols: List[str], conditions: Conditions
+    ):
         """
         returns list of rows (satisfying `condition` if provided) from `table_name` 
-        """   
+        """
 
-        condition_exp=self.make_condition_expression(conditions)
-        if len(cols)==0:
-            col_exp = '*'
+        condition_exp = self.make_condition_expression(conditions)
+        if len(cols) == 0:
+            col_exp = "*"
         else:
-            col_exp = ', '.join([f"{col}" for col in cols])
-        
-        query = ( f"SELECT {col_exp} FROM {table_name} " 
-                + (f" WHERE {condition_exp}" if condition_exp is not None else "")
-            )
+            col_exp = ", ".join([f"{col}" for col in cols])
+
+        query = f"SELECT {col_exp} FROM {table_name} " + (
+            f" WHERE {condition_exp}" if condition_exp is not None else ""
+        )
 
         async with self.parent.lock:
-            with self.parent.db:    
+            with self.parent.db:
                 self.parent.cursor.execute(query)
                 results = self.parent.cursor.fetchall()
 
         return results
-    
-    async def remove(self,table_name:str,conditions:Conditions):
-        condition_exp=self.make_condition_expression(conditions)
-        query =( f"DELETE FROM {table_name} " 
-                + (f" WHERE {condition_exp}" if condition_exp is not None else "")
-            )
+
+    async def remove(self, table_name: str, conditions: Conditions):
+        condition_exp = self.make_condition_expression(conditions)
+        query = f"DELETE FROM {table_name} " + (
+            f" WHERE {condition_exp}" if condition_exp is not None else ""
+        )
         async with self.parent.lock:
-            with self.parent.db:    
+            with self.parent.db:
                 self.parent.cursor.execute(query)
-        
-        return  
 
+        return
 
-    
 
 @entry_point("sqlitedb")
 class SqliteDatabase(BaseDatabaseObject):
     CONTEXT = SqliteDatabaseContext
     CONFIG = SqliteDatabaseConfig
 
-    def __init__(self,cfg):
+    def __init__(self, cfg):
         super().__init__(cfg)
         self.db = sqlite3.connect(self.config.filename)
-        self.cursor=self.db.cursor()
-        self.lock=asyncio.Lock()
-
-    
- 
-
-
-
+        self.cursor = self.db.cursor()
+        self.lock = asyncio.Lock()
