aca72f806d6f60d26fa5143f2238cd0a94547e22
Aghin Shah Alin <aghinsa@gmail.com>
2020-01-02 19:36:16 +0530

    comments

--- dffml/operation/mediator.py
+++ dffml/operation/mediator.py
@@ -1,18 +1,22 @@
 from typing import Dict, List, Any,Callable
-from ..df.types import Definition,Dataflow
+from ..df.types import Definition,DataFlow
 from ..df.base import op
 
 
 
-def mediator(dataflow,mapping_data,extra_defintions=[]):
+def mediator(dataflow,mapping_data,extra_definitions={}):
     """
+        creates and adds operations to `dataflow` to connect operations differing
+        in output -> input definitions,or to reformat output of one operation as
+        input of other.
 
+        eg:- `mapping_data`
             {
                 "mapping_name" :
                     {
-                        'input_types' : [("results",flow_results")]
-                        "redirect_function" : some callable
-                        "output_types" : [("name":"flow_outputs")]
+                        'input_types' : [("results","flow_results")]
+                        "redirect_function" : lambda x : {"input for some other op":results}
+                        "output_types" : [("input for some other op":"definition name of the same")]
                         "kwargs" : {}
 
                     }
@@ -20,34 +24,29 @@ def mediator(dataflow,mapping_data,extra_defintions=[]):
             }
 
     """
-    df_definitions=dataflow.definitions
-    df_definitions.extend(extra_definitions)
+    df_definitions = dataflow.definitions
+    extra_definitions.update(df_definitions)
     df_ops = dataflow.operations
     df_op_imps = dataflow.implementations
 
-    created_ops = []
-    created_op_imps = []
     for mapping_name,contents in mapping_data.items():
-        # build inputs for the op
-        op_inputs = { val[0]:val[1] for val in contents['input_types']}
-        op_outputs = { val[0]:val[1] for val in contents['output_types']}
+        # build inputs,outputs for the connecting operation
+        op_inputs = { val[0]:extra_definitions[val[1]] for val in contents['input_types']}
+        op_outputs = { val[0]:extra_definitions[val[1]] for val in contents['output_types']}
         temp_op = op(
                     name=mapping_name,
                     inputs=op_inputs,
                     outputs=op_outputs,
-                    **contents["kwargs"]
+                    **contents.get("kwargs",{})
                     )(contents['redirect_function'])
-        created_ops.append(temp_op)
-        created_op_imps.append(temp_op.op)
 
-    df_ops.extend(created_ops)
-    df_op_imps.extend(created_op_imps)
+        df_ops[mapping_name]=temp_op
+        df_op_imps[mapping_name]=temp_op.imp
 
     new_dataflow = DataFlow(
         operations = df_ops,
-        seed = dataflow.seed
+        seed = dataflow.seed,
         configs = dataflow.configs,
-        definitions=df_definitions,
         implementations = df_op_imps
     )
     return new_dataflow
