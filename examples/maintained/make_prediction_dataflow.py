c9bda73ed982c1423e2928ac29a1072b9e21dc65
John Andersen <johnandersenpdx@gmail.com>
2020-01-13 15:17:32 -0800

    examples: maintained: Move prediction df to global scope
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- examples/maintained/make_prediction_dataflow.py
+++ examples/maintained/make_prediction_dataflow.py
@@ -62,144 +62,146 @@ TODO
 
 dataflow_yaml = "./cgi-bin/dataflow.yaml"
 
+prediction_df = DataFlow(
+    operations={
+        "run_dataflow": run_dataflow.op,
+        "get_single": GetSingle.imp.op,
+        "model_predict": model_predict.op,
+        "mapping_expand_all_values": mapping_expand_all_values.op,
+        "mapping_expand_all_keys": mapping_expand_all_keys.op,
+        "mapping_extract_value": mapping_extract_value.op,
+        "create_src_url_mapping": create_mapping.op,
+        "create_maintained_mapping": create_mapping.op,
+        "create_insert_data": mapping_merge.op,
+        "conditions_array_create": array_create.op,
+        "conditions_array_append_1": array_append.op,
+        "conditions_array_append_2": array_append.op,
+        "conditions_or": array_create.op,
+        "conditions_and": array_create.op,
+        "insert_db": db_query_insert.op,
+    },
+    configs={
+        "model_predict": ModelPredictConfig(
+            model=DNNClassifierModel(
+                DNNClassifierModelConfig(
+                    predict=DefFeature("maintained", int, 1),
+                    classifications=[0, 1],
+                    features=Features(
+                        DefFeature("authors", int, 10),
+                        DefFeature("commits", int, 10),
+                        DefFeature("work", int, 10),
+                    ),
+                )
+            )
+        ),
+        "insert_db": DatabaseQueryConfig(
+            database=MySQLDatabase(
+                MySQLDatabaseConfig(
+                    host="127.0.0.1",
+                    port=3306,
+                    user="user",
+                    password="pass",
+                    db="db",
+                    ca=None,
+                )
+            )
+        ),
+    },
+    seed=[
+        # Make the output of the dataflow the prediction
+        Input(
+            value=[create_mapping.op.outputs["mapping"].name],
+            definition=GetSingle.op.inputs["spec"],
+        ),
+        # # model_predict outputs: {'model_predictions': {'maintained':
+        # {'confidence': 0.9989588260650635, 'value': '1'}}}
+        # # we need to extract the 'value' from it.
+        Input(
+            value=["maintained", "value"],
+            definition=mapping_extract_value.op.inputs["traverse"],
+        ),
+        # # Create a key value mapping where the key is "value"
+        # # {'maintained': 1}
+        Input(
+            value="src_url",
+            definition=create_mapping.op.inputs["key"],
+            origin="seed.create_src_url_mapping.key",
+        ),
+        Input(
+            value="maintained",
+            definition=create_mapping.op.inputs["key"],
+            origin="seed.create_maintained_mapping.key",
+        ),
+        # # The table to insert
+        Input(
+            value="status",
+            definition=db_query_insert.op.inputs["table_name"],
+        ),
+    ],
+    implementations={
+        "model_predict": model_predict.imp,
+        mapping_expand_all_values.op.name: mapping_expand_all_values.imp,
+        mapping_expand_all_keys.op.name: mapping_expand_all_keys.imp,
+        create_mapping.op.name: create_mapping.imp,
+        mapping_extract_value.op.name: mapping_extract_value.imp,
+        mapping_merge.op.name: mapping_merge.imp,
+        array_create.op.name: array_create.imp,
+        array_append.op.name: array_append.imp,
+        db_query_insert.op.name: db_query_insert.imp,
+    },
+)
+
+# Redirect output of run_dataflow to model_predict
+prediction_df.flow["mapping_expand_all_keys"].inputs["mapping"] = [
+    {"run_dataflow": "results"}
+]
+prediction_df.flow["mapping_expand_all_values"].inputs["mapping"] = [
+    {"run_dataflow": "results"}
+]
+prediction_df.flow["model_predict"].inputs["features"] = [
+    {"mapping_expand_all_values": "value"}
+]
+prediction_df.flow["mapping_extract_value"].inputs["mapping"] = [
+    {"model_predict": "prediction"}
+]
+
+# Create src_url mapping
+prediction_df.flow["create_src_url_mapping"].inputs["key"] = [
+    "seed.create_src_url_mapping.key"
+]
+prediction_df.flow["create_src_url_mapping"].inputs["value"] = [
+    {"mapping_expand_all_keys": "key"}
+]
+# Create maintined mapping
+prediction_df.flow["create_maintained_mapping"].inputs["key"] = [
+    "seed.create_maintained_mapping.key"
+]
+prediction_df.flow["create_maintained_mapping"].inputs["value"] = [
+    {"mapping_extract_value": "value"}
+]
+# Merge src_url and maintained mappings to create data for insert
+prediction_df.flow["create_insert_data"].inputs["one"] = [
+    {"create_src_url_mapping": "mapping"}
+]
+prediction_df.flow["create_insert_data"].inputs["two"] = [
+    {"create_maintained_mapping": "mapping"}
+]
+# Use key value mapping as data for db insert
+prediction_df.flow["insert_db"].inputs["data"] = [
+    {"create_insert_data": "mapping"},
+]
+
+prediction_df.update_by_origin()
 
 async def main():
     async with ConfigLoaders() as cfgl:
-        _,data_df = await cfgl.load_file(filepath=dataflow_yaml)
-    data_df=DataFlow._fromdict(**data_df)
-    prediction_df = DataFlow(
-            operations={
-                "run_dataflow": run_dataflow.op,
-                "get_single": GetSingle.imp.op,
-                "model_predict": model_predict.op,
-                "mapping_expand_all_values": mapping_expand_all_values.op,
-                "mapping_expand_all_keys": mapping_expand_all_keys.op,
-                "mapping_extract_value": mapping_extract_value.op,
-                "create_src_url_mapping": create_mapping.op,
-                "create_maintained_mapping": create_mapping.op,
-                "create_insert_data": mapping_merge.op,
-                "conditions_array_create": array_create.op,
-                "conditions_array_append_1": array_append.op,
-                "conditions_array_append_2": array_append.op,
-                "conditions_or": array_create.op,
-                "conditions_and": array_create.op,
-                "insert_db": db_query_insert.op,
-            },
-            configs={
-                "run_dataflow": RunDataFlowConfig(dataflow=data_df),
-                "model_predict": ModelPredictConfig(
-                    model = DNNClassifierModel(
-                        DNNClassifierModelConfig(
-                            predict = DefFeature("maintained",int,1),
-                            classifications = [0, 1],
-                            features= Features(
-                                DefFeature("authors",int,10),
-                                DefFeature("commits",int,10),
-                                DefFeature("work",int,10),
-                                )
-                            )
-                        )
-                    ),
-                "insert_db": DatabaseQueryConfig(
-                    database=MySQLDatabase(
-                        MySQLDatabaseConfig(
-                            host="127.0.0.1",
-                            port=3306,
-                            user="user",
-                            password="pass",
-                            db="db",
-                            ca=None,
-                        )
-                    )
-                ),
-            },
-            seed=[
-                # Make the output of the dataflow the prediction
-                Input(
-                    value=[create_mapping.op.outputs["mapping"].name],
-                    definition=GetSingle.op.inputs["spec"],
-                ),
-                # # model_predict outputs: {'model_predictions': {'maintained':
-                # {'confidence': 0.9989588260650635, 'value': '1'}}}
-                # # we need to extract the 'value' from it.
-                Input(
-                    value=["maintained","value"],
-                    definition=mapping_extract_value.op.inputs["traverse"],
-                ),
-                # # Create a key value mapping where the key is "value"
-                # # {'maintained': 1}
-                Input(
-                    value="src_url",
-                    definition=create_mapping.op.inputs["key"],
-                    origin="seed.create_src_url_mapping.key",
-                ),
-                Input(
-                    value="maintained",
-                    definition=create_mapping.op.inputs["key"],
-                    origin="seed.create_maintained_mapping.key",
-                ),
-                # # The table to insert
-                Input(
-                    value='status',
-                    definition=db_query_insert.op.inputs["table_name"],
-                ),
-            ],
-            implementations={
-                "model_predict": model_predict.imp,
-                mapping_expand_all_values.op.name: mapping_expand_all_values.imp,
-                mapping_expand_all_keys.op.name: mapping_expand_all_keys.imp,
-                create_mapping.op.name: create_mapping.imp,
-                mapping_extract_value.op.name: mapping_extract_value.imp,
-                mapping_merge.op.name: mapping_merge.imp,
-                array_create.op.name: array_create.imp,
-                array_append.op.name: array_append.imp,
-                db_query_insert.op.name: db_query_insert.imp,
-            },
-        )
+        _, data_df = await cfgl.load_file(filepath=dataflow_yaml)
+        data_df = DataFlow._fromdict(**data_df)
 
-
-        # Redirect output of run_dataflow to model_predict
-    prediction_df.flow["mapping_expand_all_keys"].inputs["mapping"] = [
-        {"run_dataflow": "results"}
-    ]
-    prediction_df.flow["mapping_expand_all_values"].inputs["mapping"] = [
-        {"run_dataflow": "results"}
-    ]
-    prediction_df.flow["model_predict"].inputs["features"] = [
-        {"mapping_expand_all_values": "value"}
-    ]
-    prediction_df.flow["mapping_extract_value"].inputs["mapping"] = [
-        {"model_predict": "prediction"}
-    ]
-
-    # Create src_url mapping
-    prediction_df.flow["create_src_url_mapping"].inputs["key"] = [
-        "seed.create_src_url_mapping.key"
-    ]
-    prediction_df.flow["create_src_url_mapping"].inputs["value"] = [
-        {"mapping_expand_all_keys": "key"}
-    ]
-    # Create maintined mapping
-    prediction_df.flow["create_maintained_mapping"].inputs["key"] = [
-        "seed.create_maintained_mapping.key"
-    ]
-    prediction_df.flow["create_maintained_mapping"].inputs["value"] = [
-        {"mapping_extract_value": "value"}
-    ]
-    # Merge src_url and maintained mappings to create data for insert
-    prediction_df.flow["create_insert_data"].inputs["one"] = [
-        {"create_src_url_mapping": "mapping"}
-    ]
-    prediction_df.flow["create_insert_data"].inputs["two"] = [
-        {"create_maintained_mapping": "mapping"}
-    ]
-    # Use key value mapping as data for db insert
-    prediction_df.flow["insert_db"].inputs["data"] = [
-        {"create_insert_data": "mapping"},
-    ]
-
-    prediction_df.update_by_origin()
-
+    # Add the git feature scraping dataflow as the subflow
+    prediction_df.configs["run_dataflow"] = RunDataFlowConfig(
+        dataflow=data_df,
+    )
 
     test_inputs = [
         {
@@ -228,7 +230,5 @@ async def main():
             ):
                 print(f"result are :{results}")
 
-
-
-#Run
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
