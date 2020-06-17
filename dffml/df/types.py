54501f8632e6b48cfa96f7849bb3011e6e7a7c5f
Aghin Shah Alin <aghinsa@gmail.com>
2020-06-17 17:26:51 +0530

    base: Fix loading of nested configs in _fromdict

--- dffml/df/types.py
+++ dffml/df/types.py
@@ -20,7 +20,6 @@ from ..base import BaseConfig
 from ..util.data import export_dict, type_lookup
 from ..util.entrypoint import load as load_entrypoint
 from ..util.entrypoint import Entrypoint, base_entry_point
-from ..util.entrypoint import load as load_entrypoint
 
 
 class DefinitionMissing(Exception):
