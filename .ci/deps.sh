46a7de3eda648330d0ce78953826aca7e1d2d827
John Andersen <johnandersenpdx@gmail.com>
2020-09-16 13:30:09 -0700

    remove deps.sh
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- .ci/deps.sh
+++ .ci/deps.sh
@@ -9,167 +9,6 @@ set -ex
 
 export PLUGIN="${1}"
 
-if [ "x$(id -u)" != "x0" ]; then
-  SUDO="sudo"
-fi
-
-if [ "x${PIP_CACHE_DIR}" != "x" ]; then
-  mkdir -p "${PIP_CACHE_DIR}"
-fi
-
-# Get the python version in the format of pyMajorMinor, example: py37
-python_version="py$(python -c 'import sys; print(f"{sys.version_info.major}{sys.version_info.minor}")')"
-
-export PATH="${PIP_CACHE_DIR}/miniconda${python_version}/bin:$PATH"
-
-# True or False for if `conda` is in the PATH
-has_conda=$(python -c 'import pathlib, os; print(any(map(lambda path: pathlib.Path(path, "conda").is_file(), os.environ.get("PATH", "").split(":"))))')
-
-mkdir -p "${HOME}/.local/bin"
-
-
-# ========================== BEGIN GLOBAL DEPENDENCIES =========================
-#
-# Dependencies that are applicable to the main package and plugins, or just must
-# be installed first.
-
-# Install conda because some plugins have dependencies which are only available
-# on conda (those listed first). Also because we need to install those packages
-# for the integration tests for the main package (.) and when generating the
-# docs. Has to be installed first because other packages will be installed into
-# the environment that we set up using it (essentially a virtualenv)
-if [[ "x${PLUGIN}" == "xmodel/daal4py" ]] || \
-   [[ "x${PLUGIN}" == "xmodel/vowpalWabbit" ]] || \
-   [[ "x${PLUGIN}" == "x." ]] || \
-   [[ "x${PLUGIN}" == "xdocs" ]]; then
-  if [[ "${has_conda}" != "True" ]]; then
-    # URL of conda
-    conda_url="https://repo.anaconda.com/miniconda/Miniconda3-${python_version}_4.8.2-Linux-x86_64.sh"
-    # Location to download conda to
-    conda_download="${PIP_CACHE_DIR}/conda${python_version}.sh"
-    # Hash of conda download
-    if [ "${python_version}" == "py37" ]; then
-      conda_hash=957d2f0f0701c3d1335e3b39f235d197837ad69a944fa6f5d8ad2c686b69df3b
-    elif [ "${python_version}" == "py38" ]; then
-      conda_hash=5bbb193fd201ebe25f4aeb3c58ba83feced6a25982ef4afa86d5506c3656c142
-    fi
-    # Download it
-    if [ ! -f "${conda_download}" ]; then
-      curl -L "${conda_url}" -o "${conda_download}"
-    fi
-    # Verify the hash
-    sha256sum "${conda_download}" | grep "^${conda_hash}"
-    # Run it
-    bash "${conda_download}" -b -p "${PIP_CACHE_DIR}/miniconda${python_version}"
-    # Update
-    conda update -y -n base -c defaults conda
-    # Add channels
-    conda config --add channels anaconda
-    conda config --add channels conda-forge
-    # Remove numpy 1.19.1 see https://github.com/intel/dffml/issues/816
-    conda uninstall numpy
-    conda install numpy==1.18.5
-  fi
-  if [ -f "${PIP_CACHE_DIR}/miniconda${python_version}/bin/activate" ]; then
-    source "${PIP_CACHE_DIR}/miniconda${python_version}/bin/activate" base
-  fi
-fi
-
-# Install and upgrade
-# pip and setuptools, which are used to install other packages
-# twine, which is used to upload released packages to PyPi
-python -m pip install --upgrade pip setuptools twine
-
-# Install main package so that other packages have access to it
-python -m pip install -U -e .[dev]
-
-# ==========================  END  GLOBAL DEPENDENCIES =========================
-
-
-# =========================== BEGIN TEST DEPENDENCIES ==========================
-#
-# Dependencies for specific plugins only used when running the tests for those
-# plugins. Not when generating docs or running main package integration tests
-
-if [[ "x${PLUGIN}" == "xfeature/git" ]] || \
-   [[ "x${PLUGIN}" == "xoperations/deploy" ]]; then
-  curl -sSL https://github.com/XAMPPRocky/tokei/releases/download/v9.1.1/tokei-v9.1.1-x86_64-unknown-linux-gnu.tar.gz | tar xvz -C "$HOME/.local/bin/"
-  ${SUDO} apt-get update && ${SUDO} apt-get install -y git subversion cloc openssl
-fi
-
-if [ "${PLUGIN}" == "source/mysql" ]; then
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | ${SUDO} apt-key add -
-  ${SUDO} add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-  ${SUDO} apt-get update && ${SUDO} apt-get install -y docker-ce docker-ce-cli containerd.io
-  docker pull mariadb:10
-fi
-
-# ===========================  END  TEST DEPENDENCIES ==========================
-
-
-# ========================== BEGIN INSTALL DEPENDENCIES ========================
-#
-# Dependencies which must be installed prior to installing a plugin. If a plugin
-# requires something be installed, it must also ensure that those dependencies
-# get installed when we are running the tests for the main package (.) or the
-# docs (docs). Each if statement seen here will check if we are running tests
-# for the plugin, main package, or docs, and install if any of those conditions
-# are true.
-
-if [[ "x${PLUGIN}" == "xmodel/vowpalWabbit" ]] || \
-   [[ "x${PLUGIN}" == "x." ]] || \
-   [[ "x${PLUGIN}" == "xdocs" ]]; then
-  set +e
-  # conda sometimes is a bash function, which does not abide by strict error
-  # checking, so we have to turn off exit on error and only exit if the return
-  # code of the conda bash function is non-zero
-  conda install -y -c conda-forge vowpalwabbit
-  exit_code=$?
-  if [[ "x${exit_code}" != "x0" ]]; then
-    exit "${exit_code}"
-  fi
-  set -e
-fi
-
-if ([[ "x${PLUGIN}" == "xmodel/daal4py" ]] || \
-    [[ "x${PLUGIN}" == "x." ]] || \
-    [[ "x${PLUGIN}" == "xdocs" ]]) &&
-  [[ "${python_version}" != "py38" ]]; then
-  # daal4py only supports ^ Python 3.7
-  set +e
-  # See comment in vowpalWabbit about conda exit codes
-  # See https://github.com/intel/dffml/issues/801 for discussion on pinning
-  conda install -y -c intel daal4py==2020.1 daal==2020.1
-  exit_code=$?
-  if [[ "x${exit_code}" != "x0" ]]; then
-    exit "${exit_code}"
-  fi
-  set -e
-fi
-
-if [[ "x${PLUGIN}" == "xoperations/nlp" ]]; then
-  # See comment in vowpalWabbit about conda exit codes
-  set +e
-  conda install -y -c conda-forge spacy
-  exit_code=$?
-  if [[ "x${exit_code}" != "x0" ]]; then
-    exit "${exit_code}"
-  fi
-  set -e
-  python -m spacy download en_core_web_sm
-fi
-
-if [[ "x${PLUGIN}" == "xmodel/autosklearn" ]] || \
-   [[ "x${PLUGIN}" == "x." ]] || \
-   [[ "x${PLUGIN}" == "xdocs" ]]; then
-  ${SUDO} apt-get install -y build-essential swig
-  curl https://raw.githubusercontent.com/automl/auto-sklearn/master/requirements.txt |
-    xargs -n 1 -L 1 pip install
-fi
-
-# ==========================  END  INSTALL DEPENDENCIES ========================
-
-
 # =========================== BEGIN INTER DEPENDENCIES =========================
 #
 # Core plugins which depend on other code plugins should install those core
@@ -179,18 +18,11 @@ if [ "x${PLUGIN}" == "xmodel/tensorflow_hub" ]; then
   python -m pip install -U -e "./model/tensorflow"
 fi
 
-if [[ "x${PLUGIN}" == "xmodel/spacy" ]]; then
-  conda install -y -c conda-forge spacy
-  python -m spacy download en_core_web_sm
-fi
-
 if [[ "x${PLUGIN}" == "xoperations/deploy" ]]; then
   python -m pip install -U -e "./feature/git"
 fi
 
 if [[ "x${PLUGIN}" == "xoperations/nlp" ]]; then
-  conda install -y -c conda-forge spacy
-  python -m spacy download en_core_web_sm
   python -m pip install -U -e "./model/tensorflow"
 fi
 
--- .ci/docker-entrypoint.sh
+++ .ci/docker-entrypoint.sh
@@ -11,27 +11,19 @@ if [ "x${USER}" == "x" ]; then
   export USER=user
 fi
 
+if [ "x${HOME}" == "x" ]; then
+  export HOME="/tmp/home/${USER}"
+  mkdir -p "${HOME}"
+fi
+
 echo "#!/usr/bin/env bash" > /tmp/cmd.sh
 chmod 755 /tmp/cmd.sh
 runit () {
   exec /tmp/cmd.sh
 }
 
-export VIRTUAL_ENV_DIR=$(mktemp -d)
-
-"${PYTHON}" -m venv "${VIRTUAL_ENV_DIR}"
-. "${VIRTUAL_ENV_DIR}/bin/activate"
-
-export HOME=$(mktemp -d)
-
-if [ -d "/home/${USER}/.cache/pip" ]; then
-  export PIP_CACHE_DIR="/home/${USER}/.cache/pip"
-fi
-
-mkdir -p "${HOME}/.cache"
-mkdir -p "${HOME}/.local/bin"
-
-export PATH="${HOME}/.local/bin:${PATH}"
+CONDA_INSTALL_LOCATION=/opt/conda
+. "${CONDA_INSTALL_LOCATION}/miniconda${PYTHON_SHORT_VERSION}/bin/activate" base
 
 if [ "x${1}" == "x" ]; then
   echo "exec bash" >> /tmp/cmd.sh
@@ -39,6 +31,17 @@ else
   echo "./.ci/run.sh ${1}" >> /tmp/cmd.sh
 fi
 
-source ./.ci/deps.sh "${1}"
+# source ./.ci/deps.sh "${1}"
+
+# Copy pre-downloaded containers to user accessable location
+mkdir -p "${HOME}/.local/share/"
+cp -rup /var/lib/containers "${HOME}/.local/share/containers"
+chown "${USER}:${USER}" -R "${HOME}/.local/share/containers"
+# Refresh bolt_state.db to not point to /var/run/libpod/ for it's lock file
+# Do this by removing the .db
+rm "${HOME}/.local/share/containers/storage/libpod/bolt_state.db"
+# And re-pulling the image to update the .db. Image won't be transfered over the
+# network, just revalidated and the .db updated.
+podman pull mariadb:10.5.5
 
 runit
