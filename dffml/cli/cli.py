e3b8970dfb351fdb937722f8718fbb3a104795c6
John Andersen <johnandersenpdx@gmail.com>
2021-02-13 16:16:11 -0800

    cli: packages: List all dffml packages
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/cli/cli.py
+++ dffml/cli/cli.py
@@ -18,7 +18,7 @@ from ..version import VERSION
 from ..record import Record
 from ..feature.feature import Features
 from ..df.types import DataFlow
-from ..plugins import PACKAGE_NAMES_BY_PLUGIN
+from ..plugins import PACKAGE_NAMES_BY_PLUGIN, PACKAGE_NAMES_TO_DIRECTORY
 from ..source.df import DataFlowSource, DataFlowSourceConfig
 from ..source.source import Sources, BaseSource, SubsetSources
 from ..configloader.configloader import BaseConfigLoader
@@ -155,6 +155,15 @@ class Version(CMD):
                     if dirty:
                         package_details.append("(dirty git repo)")
             print(" ".join(package_details))
+
+
+class Packages(CMD):
+    async def run(self):
+        print(
+            "\n".join(
+                sorted(["dffml"] + list(PACKAGE_NAMES_TO_DIRECTORY.keys()))
+            )
+        )
 
 
 @config
@@ -346,6 +355,7 @@ class CLI(CMD):
     """
 
     version = Version
+    packages = Packages
     _list = List
     edit = Edit
     merge = Merge
