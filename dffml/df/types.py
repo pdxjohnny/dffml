e4c16b782f36183435170b2ac9b66294b4236f56
John Andersen <johnandersenpdx@gmail.com>
2021-11-29 20:09:33 -0800

    df: types: Support Python 3.9
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/df/types.py
+++ dffml/df/types.py
@@ -1,5 +1,7 @@
+import sys
 import uuid
 import copy
+import types
 import itertools
 import pkg_resources
 from enum import Enum
@@ -134,9 +136,12 @@ class Definition(NamedTuple):
             # in dict
             for key, dtype in annotations_with_defaults.items():
                 annotations[key] = dtype
+            def_tuple["__module__"] = sys.modules[__name__]
             def_tuple["__annotations__"] = annotations
             kwargs["spec"] = type(
-                kwargs["spec"]["name"], (NamedTuple,), def_tuple
+                kwargs["spec"]["name"],
+                types.resolve_bases((NamedTuple,)),
+                def_tuple,
             )
         return cls(**kwargs)
 
