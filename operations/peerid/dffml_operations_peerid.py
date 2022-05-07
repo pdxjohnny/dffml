0404b6dc449658ea4ecb324c8f4f5522b1a438a7
John Andersen <johnandersenpdx@gmail.com>
2022-05-07 01:19:27 -0700

    Found out we are going to use this code to try to save / load from DID based blockchain that we will spin up using firefly or areis hopefully https://github.com/intel/dffml/discussions/1369#discussioncomment-2704845
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- operations/peerid/dffml_operations_peerid.py
+++ operations/peerid/dffml_operations_peerid.py
@@ -1,9 +1,15 @@
+"""
+.. code-block:: console
+
+    $ pip install peerdid
+
+"""
 # SPDX-License-Identifier: Apache-2.0
 # Source: https://github.com/sicpa-dlab/peer-did-python/blob/c63461860891d7c111abb6b24a51f23dad845a74/tests/test_vectors.py#L58-L95
 # All of these become Inputs within top level context when pointed at DID, it
 # should introspect and map to operation config (service -> orchestrator /
 # service to talk to for data).
- = {
+_EXAMPLE_PEER_DID_DOC = {
     "id": "did:peer:2.Ez6LSbysY2xFMRpGMhb7tFTLMpeuPRaqaWM1yECx2AtzE3KCc.Vz6MkqRYqQiSgvZQdnBytw86Qbs2ZWUkGv22od935YF4s8M7V.Vz6MkgoLTnTypo3tDRwCkZXSccTPHRLhF4ZnjhueYAFpEX6vg.SeyJ0IjoiZG0iLCJzIjoiaHR0cHM6Ly9leGFtcGxlLmNvbS9lbmRwb2ludCIsInIiOlsiZGlkOmV4YW1wbGU6c29tZW1lZGlhdG9yI3NvbWVrZXkiXSwiYSI6WyJkaWRjb21tL3YyIiwiZGlkY29tbS9haXAyO2Vudj1yZmM1ODciXX0",
     "authentication": [
        {
@@ -42,6 +48,77 @@
     ]
 }
 
-async def 
+# Shim:
+#   - Validate schema
+#     - Then call this if the format is DID
 
 
+async def dataflow_from_peer_did(self):
+    pass
+
+# SPDX-License-Identifier: Apache-2.0
+# Source: https://github.com/sicpa-dlab/peer-did-python/blob/c63461860891d7c111abb6b24a51f23dad845a74/demo/demo.py
+from peerdid.did_doc import DIDDocPeerDID
+from peerdid.peer_did import (
+    create_peer_did_numalgo_0,
+    create_peer_did_numalgo_2,
+    resolve_peer_did,
+)
+from peerdid.types import (
+    VerificationMaterialAuthentication,
+    VerificationMaterialAgreement,
+    VerificationMethodTypeAgreement,
+    VerificationMethodTypeAuthentication,
+    VerificationMaterialFormatPeerDID,
+)
+
+
+def demo():
+    encryption_keys = [
+        VerificationMaterialAgreement(
+            type=VerificationMethodTypeAgreement.X25519_KEY_AGREEMENT_KEY_2019,
+            format=VerificationMaterialFormatPeerDID.BASE58,
+            value="DmgBSHMqaZiYqwNMEJJuxWzsGGC8jUYADrfSdBrC6L8s",
+        )
+    ]
+    signing_keys = [
+        VerificationMaterialAuthentication(
+            type=VerificationMethodTypeAuthentication.ED25519_VERIFICATION_KEY_2018,
+            format=VerificationMaterialFormatPeerDID.BASE58,
+            value="ByHnpUCFb1vAfh9CFZ8ZkmUZguURW8nSw889hy6rD8L7",
+        )
+    ]
+    service = """
+                {
+                    "type": "DIDCommMessaging",
+                    "serviceEndpoint": "https://example.com/endpoint1",
+                    "routingKeys": ["did:example:somemediator#somekey1"],
+                    "accept": ["didcomm/v2", "didcomm/aip2;env=rfc587"]
+                }
+            """
+
+    peer_did_algo_0 = create_peer_did_numalgo_0(inception_key=signing_keys[0])
+    peer_did_algo_2 = create_peer_did_numalgo_2(
+        encryption_keys=encryption_keys, signing_keys=signing_keys, service=service
+    )
+
+    print("peer_did_algo_0:" + peer_did_algo_0)
+    print("==================================")
+    print("peer_did_algo_2:" + peer_did_algo_2)
+    print("==================================")
+
+    did_doc_algo_0_json = resolve_peer_did(peer_did=peer_did_algo_0)
+    did_doc_algo_2_json = resolve_peer_did(peer_did=peer_did_algo_2)
+    print("did_doc_algo_0 as JSON:" + did_doc_algo_0_json)
+    print("==================================")
+    print("did_doc_algo_2 as JSON:" + did_doc_algo_2_json)
+
+    did_doc_algo_0 = DIDDocPeerDID.from_json(did_doc_algo_0_json)
+    did_doc_algo_2 = DIDDocPeerDID.from_json(did_doc_algo_2_json)
+    print("did_doc_algo_0 as object:" + str(did_doc_algo_0.to_dict()))
+    print("==================================")
+    print("did_doc_algo_2 as object:" + str(did_doc_algo_2.to_dict()))
+
+
+if __name__ == "__main__":
+    demo()
