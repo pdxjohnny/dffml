12250e2f1f86ed03c02a9ff0b4051d5a1a43d6ae
John Andersen <johnandersenpdx@gmail.com>
2022-05-31 23:16:04 -0700

    util: cli: cmd: Add use of system context to kick of CLI
    Currently not working with kwargs passing to old style CMD invocation,
    unclear why. It does get us to the point where it does the CreateCMD
    instantitation and it says there is no attribute package on the CMD
    instance.
    
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- dffml/util/cli/cmd.py
+++ dffml/util/cli/cmd.py
@@ -10,11 +10,15 @@ import asyncio
 import datetime
 import argparse
 import dataclasses
-from typing import Dict, Any
+from typing import Dict, Any, Optional, NewType, Union
 import dataclasses
 
 from ...record import Record
 from ...feature import Feature
+from ...df.types import DataFlow
+from ...df.base import op
+from ...df.system_context.system_context import SystemContext
+from ...df.system_context.system_context import APPLY_INSTALLED_OVERLAYS
 
 from ..data import export_dict, merge
 from .arg import Arg, parse_unknown
@@ -86,6 +90,12 @@ log_cmd = Arg(
     required=False,
     default=logging.INFO,
 )
+overlay_cmd = Arg(
+    "-overlay",
+    help="The overlay for the top level system context",
+    required=False,
+    default=APPLY_INSTALLED_OVERLAYS,
+)
 
 
 class Parser(argparse.ArgumentParser):
@@ -99,7 +109,9 @@ class Parser(argparse.ArgumentParser):
             (name.lower().replace("_", ""), method)
             for name, method in inspect.getmembers(add_from)
         ]:
-            if inspect.isclass(method) and issubclass(method, CMD):
+            if inspect.isclass(method) and (
+                issubclass(method, CMD) or issubclass(method, SystemContext)
+            ):
                 if subparsers is None:  # pragma: no cover
                     subparsers = self.add_subparsers()  # pragma: no cover
                 parser = subparsers.add_parser(
@@ -145,6 +157,12 @@ class Parser(argparse.ArgumentParser):
         except argparse.ArgumentError:
             pass
 
+        # Add `-overlay` argument if it's not already added
+        try:
+            self.add_argument(overlay_cmd.name, **overlay_cmd)
+        except argparse.ArgumentError:
+            pass
+
 
 @config
 class CMDConfig:
@@ -154,16 +172,57 @@ class CMDConfig:
         required=False,
         action=ParseLoggingAction,
     )
+    overlay: Union[None, str, DataFlow] = field(
+        "The overlay for the top level system context",
+        default=APPLY_INSTALLED_OVERLAYS,
+        required=False,
+    )
 
 
-class CMD(object):
+ExtraConfig = NewType("dffml.util.cli.cmd.extra_config", dict)
+CMDKeywordArgs = NewType("dffml.util.cli.cmd.kwargs", dict)
+CLICMD = NewType("dffml.util.cli.cmd", object)
+# TODO CLICMDResults might Union[list, dict] or Any?
+CLICMDResults = NewType("dffml.util.cli.cmd.results", dict)
+
+
+@op
+async def run_cli_command(
+    self, cmd: CLICMD, extra_config: ExtraConfig, kwargs: CMDKeywordArgs,
+) -> CLICMDResults:
+    if not inspect.isclass(cmd):
+        raise TypeError(
+            f"Unknown cmd type. Not sure what to do with cmd: {cmd!r}"
+        )
+    if issubclass(cmd, CMD):
+        # Backwards compatability with old style CLI commands
+        cmd = cmd(**kwargs)
+        return await cmd.do_run()
+    if issubclass(cmd, SystemContext):
+        # If the command defined is a system context
+        async with self.subflow(cmd) as octx:
+            async for ctx, results in octx.run(
+                [
+                    Input(value=extra_config, definition=ExtraConfig),
+                    Input(value=kwargs, definition=CMDKeywordArgs),
+                    Input(value=cmd, definition=CLICMD),
+                ],
+            ):
+                return results
+    raise TypeError(f"Unknown cmd type. Not sure what to do with cmd: {cmd!r}")
+
+
+class CMD(BaseDataFlowFacilitatorObject):
 
     JSONEncoder = JSONEncoder
     EXTRA_CONFIG_ARGS = {}
     CONFIG = CMDConfig
     ENTRY_POINT_NAME = ["service"]
+    SYSTEM_CONTEXT = SystemContext(upstream=DataFlow(run_cli_command),)
 
-    def __init__(self, extra_config=None, **kwargs) -> None:
+    def __init__(
+        self, extra_config: Optional[ExtraConfig] = None, **kwargs
+    ) -> None:
         if not hasattr(self, "logger"):
             self.logger = logging.getLogger(
                 "%s.%s"
@@ -216,7 +275,7 @@ class CMD(object):
     async def cli(cls, *args):
         parser, (args, unknown) = await cls.parse_args(*args)
         async with ConfigLoaders() as configloaders:
-            args.extra_config = await parse_unknown(
+            extra_config = await parse_unknown(
                 *unknown, configloaders=configloaders
             )
         if (
@@ -230,15 +289,21 @@ class CMD(object):
         if not inspect.isfunction(getattr(args.cmd, "run", None)):
             args.parser.print_help()
             return DisplayHelp
-        cmd = args.cmd(**cls.sanitize_args(vars(args)))
-        return await cmd.do_run()
+        # Run the top level system context for the CLI
+        cmd = args.cmd
+        async for _ctx, results in cls.SYSTEM_CONTEXT.deployment()(
+            extra_config=extra_config,
+            kwargs=cls.sanitize_args(vars(args)),
+            cmd=cmd,
+        ):
+            return results
 
     @classmethod
     def sanitize_args(cls, args):
         """
         Remove CMD internals from arguments passed to subclasses of CMD.
         """
-        for rm in ["cmd", "parser", "log"]:
+        for rm in ["cmd", "parser", "log", "overlay"]:
             if rm in args:
                 del args[rm]
         return args
