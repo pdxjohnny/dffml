db588d8e7df8b495f7cdb3b20acb2f8fc5afd9b8
John Andersen <johnandersenpdx@gmail.com>
2022-06-21 06:54:00 -0700

    alice: cli: please: contribute: Attempt and fail to build single dataflow from all classes
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- entities/alice/alice/cli.py
+++ entities/alice/alice/cli.py
@@ -393,6 +393,7 @@ class AlicePleaseContributeCLI(dffml.CMD):
             """
         ).lstrip()
 
+        # TODO Use overlays instead of combining all classes into one
         async for ctx, results in dffml.run(
             dffml.DataFlow(
                 *itertools.chain(
@@ -408,7 +409,11 @@ class AlicePleaseContributeCLI(dffml.CMD):
                             )
                         ]
                         for cls in [
+                            AlicePleaseContributeRecommendedCommunityStandards,
+                            AlicePleaseContributeRecommendedCommunityStandardsGit,
                             AlicePleaseContributeRecommendedCommunityStandardsCLIOverlay,
+                            AlicePleaseContributeRecommendedCommunityStandardsGitHubIssueOverlay,
+                            AlicePleaseContributeRecommendedCommunityStandardsGitHubPullRequestOverlay,
                         ]
                     ]
                 )
