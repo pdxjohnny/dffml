40e3c6f98958e8ee9bdd36da8e82cc4aec499131
John Andersen <johnandersenpdx@gmail.com>
2023-09-25 08:54:32 +0000

    feature: git: Remove leading whitespace changes from patchsets
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

---
+++ feature/git/tests/test_patch.py
@@ -0,0 +1,102 @@
+# git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -
+TEST_OLD_PATCHSET_WITH_WHITESPACE_CHANGE = """diff --git a/.github/workflows/testing.yml b/.github/workflows/testing.yml
+index 20da85c6f..5fc5fd305 100644
+--- a/.github/workflows/testing.yml
++++ b/.github/workflows/testing.yml
+@@ -23,7 +23,7 @@ jobs:
+         - "3.9"
+ 
+     steps:
+-      - uses: actions/checkout@v2
++    - uses: actions/checkout@v4
+       - name: Checkout full upstream repo
+         run: |
+           git remote set-url origin https://github.com/intel/dffml
+"""
+TEST_NEW_PATCHSET_WITH_WHITESPACE_CHANGE = """diff --git a/.github/workflows/testing.yml b/.github/workflows/testing.yml
+index 20da85c6f..5fc5fd305 100644
+--- a/.github/workflows/testing.yml
++++ b/.github/workflows/testing.yml
+@@ -23,7 +23,7 @@ jobs:
+         - "3.9"
+ 
+     steps:
+-      - uses: actions/checkout@v2
++      - uses: actions/checkout@v4
+       - name: Checkout full upstream repo
+         run: |
+           git remote set-url origin https://github.com/intel/dffml
+"""
+import unittest
+import pprint
+
+import io
+import contextlib
+from typing import Union, IO
+
+from unidiff import PatchSet
+
+
+def without_whitespace_changes(old_patchset: Union[IO, str]) -> str:
+    with contextlib.ExitStack() as exit_stack:
+        # Support for string or file object as input
+        if isinstance(old_patchset, str):
+            old_patchset = exit_stack.enter_context(io.StringIO(old_patchset))
+        patchset = PatchSet(old_patchset)
+    for patch in patchset:
+        # print(hunk)
+        for hunk in patch:
+            # print(line)
+            added = []
+            removed = []
+            current_added = []
+            current_removed = []
+            for line in hunk:
+                if line.line_type not in ("+", "-"):
+                    continue
+                # TODO Support for only added or just removed, rather than just
+                # modifications>
+                if (
+                    current_added
+                    and current_removed
+                    and line.target_line_no is None
+                ):
+                    added.append(current_added)
+                    removed.append(current_removed)
+                    current_added = []
+                    current_removed = []
+                # line.value = line.value.rstrip()
+                (
+                    current_added if line.line_type == "+" else current_removed
+                ).append(line)
+            if current_added and current_removed:
+                added.append(current_added)
+                removed.append(current_removed)
+            for current_added, current_removed in zip(added, removed):
+                for (source_i, source_line), (target_i, target_line) in zip(
+                    enumerate(current_removed),
+                    enumerate(current_added),
+                ):
+                    # TODO Handle more than left indent changes
+                    source_left_indent_count = len(source_line.value) - len(
+                        source_line.value.lstrip()
+                    )
+                    target_left_indent_count = len(target_line.value) - len(
+                        target_line.value.lstrip()
+                    )
+                    tab_or_space = source_line.value[0]
+                    if source_left_indent_count != target_left_indent_count:
+                        target_line.value = (
+                            tab_or_space * source_left_indent_count
+                            + target_line.value.lstrip()
+                        )
+    return str(patchset)
+
+
+class TestWithoutWhitespaceChanges(unittest.TestCase):
+    def test_indent_spaces(self):
+        self.maxDiff = None
+        old_patchset = TEST_OLD_PATCHSET_WITH_WHITESPACE_CHANGE
+        new_patchset_should_be = TEST_NEW_PATCHSET_WITH_WHITESPACE_CHANGE
+        new_patchset = without_whitespace_changes(old_patchset)
+        self.assertEqual(new_patchset, new_patchset_should_be)
